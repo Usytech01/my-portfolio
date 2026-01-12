55bf3ca9eb45a080fa3f4ac9e206858ea049c0ed
Usytech01 <usmandioo@yahoo.com>
2026-01-12 14:19:30 +0100

    first commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+// next.config.ts here and there
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+export default nextConfig;
